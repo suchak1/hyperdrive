79232ae44a303adeffa2480c0f2e13e640470f5e
Krish Suchak <suchak.krish@gmail.com>
2020-06-12 23:15:35 -0400

    abstract a few functions out

--- scarlett.py
+++ scarlett.py
@@ -4,14 +4,11 @@ import os
 import robin_stocks as rh
 from dotenv import load_dotenv
 import time
+import pandas as pd
 
 class Scarlett:
-    def __new__(cls, *args, **kwargs):
-        start = time.time()
+    def __init__(self, usr, pwd):
         # Authentication
-        if len(args) > 2:
-            raise Exception("Too many arguments. Only need email and password or store your credentials under environment variables 'EMAIL' and 'PASSWORD'.")
-        
         if usr and pwd:
             rh.login(usr, pwd)
         else:
@@ -19,21 +16,38 @@ class Scarlett:
             rh.login(
                 os.environ['EMAIL'], 
                 os.environ['PASSWORD'])
-        
+        self.rh = rh
+    
+    def get_symbols(self, instruments):
+        # given a list of instruments, 
+        # return a list of corresponding symbols
+        return [rh.stocks.get_symbol_by_url(instrument) for instrument in instruments]
+    
+    def get_hists(self, symbols, span='5year'):
+        # given a list of symbols,
+        # return a DataFrame with historical data
+        hist = self.rh.stocks.get_historicals(symbols, span)
+        df = pd.DataFrame.from_records(hist)
+        df['begins_at'] = pd.to_datetime(df['begins_at'])
+        df = df.sort_values('begins_at')
+        return df
+
+    def load_portfolio(self):
+        start = time.time()
         # Data acquisition
-        positions = rh.account.get_all_positions()
-        holdings = rh.build_holdings()
-        # need to get historicals for all instruments in positions
-        instruments = {}
-        for position in positions:
-            instrument = position['instrument']
-            symbol = rh.stocks.get_symbol_by_url(position['instrument'])
-            instruments[instrument] = symbol
-        symbols = dict(map(reversed, instruments.items()))
+        self.positions = rh.account.get_all_positions()
+        self.holdings = rh.build_holdings()
+
+        # Create lookup table instrument -> symbol and vice versa
+        instruments = [position['instrument'] for position in self.positions]
+        symbols = self.get_symbols(instruments)
         
+        self.instruments = dict(zip(instruments, symbols))
+        self.symbols = dict(map(reversed, self.instruments.items()))
+
+        # Get historical data for all instruments
+        self.hist = self.get_hists(symbols)
         end = time.time()
         print(f'Successfully loaded portfolio in {round(end-start, 2)}s.')
-        return self
-    def load_portfolio
 
 # Scarlett()
