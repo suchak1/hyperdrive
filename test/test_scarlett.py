98c858ee31737ec2f1cc3b468096d8e99ea90e1d
Krish Suchak <suchak.krish@gmail.com>
2020-06-18 03:15:12 -0400

    new tests

--- test/test_scarlett.py
+++ test/test_scarlett.py
@@ -1,6 +1,11 @@
+import os
 import sys
+import json
 sys.path.append('src')
-from scarlett import *  # nopep8
+from scarlett import \
+    flatten, save_file, load_file  # noqa autopep8
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 def test_flatten():
@@ -12,5 +17,37 @@ def test_flatten():
     assert flatten([[1, 2], [3, 4]]) == [1, 2, 3, 4]
 
 
+file_path1 = os.path.join(dir_path, 'test1.json')
+file_path2 = os.path.join(dir_path, 'test2.json')
+
+empty = {}
+data = [
+    {
+        'symbol': 'AMZN',
+        'open': '2400.00',
+        'volume': 402265
+    },
+    {
+        'symbol': 'AAPL',
+        'open': '300.00',
+        'volume': 502265
+
+    }
+]
+
+
 def test_save_file():
-    save_file('test.json', {})
+    # save empty json object
+    save_file(file_path1, {})
+    assert os.path.exists(file_path1)
+
+    # save list of 2 json objects
+    save_file(file_path2, data)
+    assert os.path.exists(file_path2)
+
+
+def test_load_file():
+    # empty case from above
+    assert load_file(file_path1) == empty
+    # mock data case from above
+    assert load_file(file_path2) == data
