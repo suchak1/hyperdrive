84a3056bc25fa98de23e411e05f7b06ff19bb4bf
Krish Suchak <suchak.krish@gmail.com>
2020-06-24 20:21:52 -0400

    split prebuild and postbuild scripts

---
+++ scripts/encrypt.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TOKEN="robinhood.pickle"
+TOKEN_DIR="${HOME}/.tokens"
+NEW_TOKEN=$(diff "${TOKEN_DIR}/${TOKEN}\
+" ${TOKEN} && echo false || echo true)
+
+# If Robinhood API grants us a new token:
+if [[ ${NEW_TOKEN} == true ]]; then 
+    # Encrypt token
+    gpg --quiet --batch --yes --symmetric --cipher-algo AES256 --passphrase="${PASSWORD}" "${TOKEN}"
+fi
+
+
+
+# Remove leftover tokens
+rm -rf "${TOKEN_DIR}"
+rm "${TOKEN}"
+
+
+    
+# if git diff (decrypted versions) and branch == master,
+# commit and push
+# master branch: daily
+# consider just running pytest init here (build.py -> starts Scarlett)
