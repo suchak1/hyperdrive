cf1241042b1edf36a2f8c2a756893a20200378a4
Krish Suchak <suchak.krish@gmail.com>
2020-07-01 02:25:51 -0400

    whyyy

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 TOKEN="robinhood.pickle"
 TOKEN_DIR="${HOME}/.tokens"
 diff "${TOKEN_DIR}/${TOKEN}" ${TOKEN}
@@ -7,8 +7,11 @@ NEW_TOKEN=$?
 
 # If Robinhood API grants us a new token:
 if [[ ${NEW_TOKEN} == 1 ]]; then 
+    echo new token
     # Encrypt token
     gpg --quiet --batch --yes --symmetric --cipher-algo AES256 --passphrase=${PASSWORD} "${TOKEN_DIR}/${TOKEN}"
+else
+    echo no change
 fi
 
 # Remove leftover tokens
