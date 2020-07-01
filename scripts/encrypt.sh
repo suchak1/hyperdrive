2d56fd6ae1d39f7242b5835a6dec0e6f65d87c92
Krish Suchak <suchak.krish@gmail.com>
2020-07-01 03:17:11 -0400

    remove comments

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -7,12 +7,8 @@ NEW_TOKEN=$?
 
 # If Robinhood API grants us a new token:
 if [[ ${NEW_TOKEN} == 1 ]]; then 
-    echo new token
-    # rm 
     # Encrypt token
     gpg --batch --yes --symmetric --cipher-algo AES256 --passphrase=${PASSWORD} --output "${TOKEN}.gpg" "${TOKEN_DIR}/${TOKEN}"
-else
-    echo no change
 fi
 
 # Remove leftover tokens
