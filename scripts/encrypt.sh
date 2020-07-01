b362a92017f718054b4264fcba82fd75cb3fe9d5
Krish Suchak <suchak.krish@gmail.com>
2020-07-01 02:20:07 -0400

    fixed issue

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-set -x
+
 TOKEN="robinhood.pickle"
 TOKEN_DIR="${HOME}/.tokens"
-NEW_TOKEN=$(cmp "${TOKEN_DIR}/${TOKEN}" ${TOKEN} && echo "false" || echo "true")
-cmp "${TOKEN_DIR}/${TOKEN}" ${TOKEN} && echo "false" || echo "true"
+$(diff "${TOKEN_DIR}/${TOKEN}" ${TOKEN})
+NEW_TOKEN=$?
+
 # If Robinhood API grants us a new token:
-if [[ "${NEW_TOKEN}" == "true" ]]; then 
-    echo new token
+if [[ ${NEW_TOKEN} == 1 ]]; then 
     # Encrypt token
     gpg --quiet --batch --yes --symmetric --cipher-algo AES256 --passphrase=${PASSWORD} "${TOKEN_DIR}/${TOKEN}"
-else
-    echo no change
 fi
 
 # Remove leftover tokens
--- scripts/login.py
+++ scripts/login.py
@@ -4,4 +4,3 @@ sys.path.append('src')
 from scarlett import Scarlett  # noqa autopep8
 
 sl = Scarlett(os.environ['EMAIL'], os.environ['PASSWORD'])
-sl = Scarlett(os.environ['EMAIL'], os.environ['PASSWORD'])
