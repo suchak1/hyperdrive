61ab05616944294ecd61c6a1b2cea41c6cf6344f
Krish Suchak <suchak.krish@gmail.com>
2020-08-02 16:34:23 -0400

    import

--- test/test_Broker.py
+++ test/test_Broker.py
@@ -3,7 +3,7 @@ import sys
 import pandas as pd
 from datetime import datetime
 sys.path.append('src')
-from Algotrader import Scarlett  # noqa autopep8
+from Broker import Robinhood  # noqa autopep8
 
 rh = Robinhood()
 exp_symbols = ['AAPL', 'FB', 'DIS']
