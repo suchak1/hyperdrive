560c57904292c3cab0ae08b70434f4aee0a4cbde
Krish Suchak <suchak.krish@gmail.com>
2020-08-05 22:00:29 -0400

    dividend workflows and weird dividend bug

--- src/Storage.py
+++ src/Storage.py
@@ -15,8 +15,8 @@ class Uploader:
             s3 = boto3.resource('s3')
             s3.Bucket('suchak1.scarlett').put_object(Key=path, Body=data)
 
-    def upload_dir(self, path):
-        paths = self.reader.get_all_paths(path)
+    def upload_dir(self, path, truncate=False):
+        paths = self.reader.get_all_paths(path, truncate=truncate)
         with Pool() as p:
             p.map(self.upload_file, paths)
 
