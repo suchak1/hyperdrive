9fdce3586838e945889270e6374b8546f578f55e
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2020-08-12 07:32:56 -0400

    💽 feat(data): s3 integration 💽 (#19)
    * fix release?
    
    * file walk
    
    * storage file
    
    * s3 dir uploader working as expected
    
    * dividend s3 upload script complete
    
    * dividend workflows and weird dividend bug
    
    * floating point precision fix
    
    * remove data dir
    
    * move pathfinder to fileops
    
    * s3 integration
    
    * add functions to store class and finish most store tests
    
    * finish store tests
    
    * tests done?
    
    * done?
    
    * linting
    
    * aws configure
    
    * make path before saving
    
    * circular import
    
    * make path
    
    * remove pathlib
    
    * fix rename
    
    * more rename fixes
    
    * does this work
    
    * done
    
    * fixed workflows

--- scripts/update_dividends.py
+++ scripts/update_dividends.py
@@ -9,8 +9,10 @@ symbols = iex.get_symbols()
 
 
 def multi_div(symbol):
+    # define fx to be pickled in multiprocessing
     iex.save_dividends(symbol)
 
 
+# save files as CSVs and uploads to S3
 with Pool() as p:
     p.map(multi_div, symbols)
