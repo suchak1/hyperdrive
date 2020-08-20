e3507697ed9fa87c779a68b24b214fff287cae3b
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2020-08-20 04:55:48 -0400

    💎 Feature: Add Polygon.io (Double Redundancy) 💎 (#23)
    * still need to write get_splits fx
    
    * made iexcloud subdirs
    
    * start polygon class
    
    * fix test
    
    * init function
    
    * switch to polygon api
    
    * auto dep check
    
    * polygon dividend redundancy added
    
    * tests
    
    * update env var in workflows
    
    * secrets
    
    * fixes?
    
    * fix test
    
    * done

--- src/DataSource.py
+++ src/DataSource.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import pandas as pd
+from polygon import RESTClient
 from dotenv import load_dotenv
 from FileOps import FileReader, FileWriter
 from Constants import PathFinder
@@ -15,28 +16,59 @@ class MarketData:
         self.writer = FileWriter()
         self.reader = FileReader()
         self.finder = PathFinder()
+        self.provider = 'iexcloud'
 
     def get_symbols(self):
         # get cached list of symbols
         symbols_path = self.finder.get_symbols_path()
         return list(self.reader.load_csv(symbols_path)[C.SYMBOL])
 
-    def get_dividends(self, symbol, timeframe=None):
+    def get_dividends(self, symbol, timeframe='max'):
         # given a symbol, return a cached dataframe
-        return self.reader.load_csv(self.finder.get_dividends_path(symbol))
+        df = self.reader.load_csv(
+            self.finder.get_dividends_path(symbol, self.provider))
+        filtered = self.reader.data_in_timeframe(df, C.EX, timeframe)
+        return filtered
 
-    def save_dividends(self, symbol, timeframe=None):
+    def standardize_dividends(self, symbol, df):
+        full_mapping = dict(
+            zip(
+                ['exDate', 'paymentDate', 'declaredDate', 'amount'],
+                [C.EX, C.PAY, C.DEC, C.DIV]
+            )
+        )
+        mapping = {k: v for k, v in full_mapping.items() if k in df}
+        columns = list(mapping)
+
+        df = df[columns].rename(columns=mapping)
+        filename = self.finder.get_dividends_path(symbol, self.provider)
+
+        if C.EX in df and C.DIV in df:
+            df = self.reader.update_df(
+                filename, df, C.EX).sort_values(by=[C.EX])
+            df[C.DIV] = df[C.DIV].apply(lambda amt: float(amt) if amt else 0)
+
+        return df
+
+    def save_dividends(self, **kwargs):
         # given a symbol, save its dividend history
-        if timeframe:
-            df = self.get_dividends(symbol, timeframe)
-        else:
-            df = self.get_dividends(symbol)
-        self.writer.update_csv(self.finder.get_dividends_path(symbol), df)
+        symbol = kwargs['symbol']
+        df = self.get_dividends(**kwargs)
+        self.writer.update_csv(
+            self.finder.get_dividends_path(symbol, self.provider), df)
 
-    # def save_splits(self, symbol):
-    #     # given a symbol, save its stock split history
-    #     df = self.get_splits(symbol)
+    # def get_splits(self, symbol, timeframe='max'):
+    #     # given a symbol, return a cached dataframe
+    #     df = self.reader.load_csv(self.finder.get_splits_path(symbol))
+    #     filtered = self.reader.data_in_timeframe(df, C.EX, timeframe)
+    #     return filtered
+
+    # def save_splits(self, **kwargs):
+    #     # given a symbol, save its splits history
+    #     symbol = kwargs['symbol']
+    #     df = self.get_splits(**kwargs)
     #     self.writer.update_csv(self.finder.get_splits_path(symbol), df)
+
 
 # make tiingo OR IEX CLOUD!! version of get dividends which
 # fetches existing dividend csv and adds a row if dividend
@@ -50,6 +82,7 @@ class IEXCloud(MarketData):
         self.base = 'https://cloud.iexapis.com'
         self.version = 'stable'
         self.token = os.environ['IEXCLOUD']
+        self.provider = 'iexcloud'
 
     def get_endpoint(self, parts):
         # given a url
@@ -84,16 +117,9 @@ class IEXCloud(MarketData):
         if not response.ok or data == []:
             return empty
 
-        columns = ['exDate', 'paymentDate', 'declaredDate', 'amount']
-        mapping = dict(zip(columns, [C.EX, C.PAY, C.DEC, C.DIV]))
-        df = pd.DataFrame(data)[columns].rename(columns=mapping)
+        df = pd.DataFrame(data)
 
-        filename = self.finder.get_dividends_path(symbol)
-
-        df = self.reader.update_df(filename, df, C.EX).sort_values(by=[C.EX])
-        df[C.DIV] = df[C.DIV].apply(lambda amt: float(amt) if amt else 0)
-
-        return df
+        return self.standardize_dividends(symbol, df)
 
     # def get_splits(self, symbol):
     #     # given a symbol, return the stock splits
@@ -104,3 +130,17 @@ class IEXCloud(MarketData):
     #     )
     #     df = df[df['Stock Splits'] != 0]
     #     return df
+
+
+class Polygon(MarketData):
+    def __init__(self, broker=None):
+        super().__init__(broker=broker)
+        load_dotenv()
+        self.client = RESTClient(os.environ['APCA_API_KEY_ID'])
+        self.provider = 'polygon'
+
+    def get_dividends(self, symbol, timeframe='max'):
+        response = self.client.reference_stock_dividends(symbol)
+        raw = pd.DataFrame(response.results)
+        df = self.standardize_dividends(symbol, raw)
+        return self.reader.data_in_timeframe(df, C.EX, timeframe)
--- src/FileOps.py
+++ src/FileOps.py
@@ -1,7 +1,7 @@
 import os
 import json
 import time
-from datetime import datetime
+from datetime import date, datetime, timedelta
 import pandas as pd
 from Storage import Store
 # consider combining fileoperations into one class
@@ -49,12 +49,47 @@ class FileReader:
     def update_df(self, filename, new, column):
         old = self.load_csv(filename)
         if not old.empty:
+            old[column] = pd.to_datetime(old[column])
+            new[column] = pd.to_datetime(new[column])
             old = old[~old[column].isin(new[column])]
             new = old.append(new, ignore_index=True)
         return new
 
     def check_file_exists(self, filename):
         return os.path.exists(filename) and self.store.key_exists(filename)
+
+    def convert_delta(self, timeframe):
+        if timeframe == 'max':
+            return timedelta(days=36500)
+
+        periods = {'y': 365, 'm': 30, 'w': 7, 'd': 1}
+        period = 'y'
+        idx = -1
+
+        for curr_period in periods:
+            idx = timeframe.find(curr_period)
+            if idx != -1:
+                period = curr_period
+                break
+
+        if idx == -1:
+            supported = ', '.join(list(periods))
+            error_msg = f'Only certain suffixes ({supported}) are supported.'
+            raise ValueError(error_msg)
+
+        num = int(timeframe[:idx])
+        days = periods[period] * num
+        delta = timedelta(days=days)
+
+        return delta
+
+    def data_in_timeframe(self, df, col, timeframe='max'):
+        if col not in df:
+            return df
+        delta = self.convert_delta(timeframe)
+        df[col] = pd.to_datetime(df[col])
+        filtered = df[df[col] > pd.to_datetime(date.today() - delta)]
+        return filtered
 
 
 class FileWriter:
