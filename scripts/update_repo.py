fedc2a64f206cce78ba87f5b93a094b3b5396136
Krish Suchak <suchak.krish@gmail.com>
2020-10-22 08:34:56 -0400

    done

--- scripts/update_repo.py
+++ scripts/update_repo.py
@@ -1,4 +1,5 @@
 import sys
+import shutil
 sys.path.append('src')
 from Storage import Store  # noqa autopep8
 from Constants import DATA_DIR  # noqa autopep8
@@ -7,4 +8,5 @@ store = Store()
 repo_keys = [key for key in store.get_all_keys()
              if key.find(f'{DATA_DIR}/')]
 store.delete_objects(repo_keys)
+shutil.rmtree(DATA_DIR)
 store.upload_dir(path='.', truncate=True)
