1f8e36db27309a3a16beb17c33a1db2e8ea3ad7a
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2020-11-05 03:06:12 -0500

    📦 chore(auto): publish package on pypi 📦 (#58)
    * done?
    
    * upgrade packages

---
+++ setup.py
@@ -0,0 +1,46 @@
+import os
+import requests
+from dotenv import load_dotenv
+from setuptools import setup, find_packages
+
+
+load_dotenv()
+
+
+def get_version():
+    url = 'https://api.github.com/repos/suchak1/hyperdrive/releases/latest'
+    token = os.environ.get('GITHUB')
+    headers = {'Authorization': f'token {token}'}
+    response = requests.get(url, headers=headers if token else None)
+    data = response.json()
+    version = data['tag_name'].replace('v', '')
+    return version
+
+
+def get_requirements():
+    with open('requirements.txt', 'r') as file:
+        return [line.strip() for line in file if line]
+
+
+def get_readme():
+    with open("README.md", "r") as file:
+        return file.read()
+
+
+setup(
+    name='hyperdrive-quant',
+    version=get_version(),
+    description='An algorithmic trading platform',
+    long_description=get_readme(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/suchak1/hyperdrive',
+    author='Krish Suchak',
+    author_email='suchak.krish@gmail.com',
+    packages=find_packages(),
+    python_requires='>=3.7',
+    install_requires=get_requirements(),
+    project_urls={
+        'Bug Reports': 'https://github.com/suchak1/hyperdrive/issues',
+        'Source': 'https://github.com/suchak1/hyperdrive'
+    }
+)
---
+++ util/git_cleanup.sh
@@ -0,0 +1,1 @@
+git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $branch; done
