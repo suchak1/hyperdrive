d25b4338bd264927070ae4c14a2f199d35945cc5
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2020-11-05 03:06:12 -0500

    📦 Feature: Publish package on PyPi 📦 (#58)
    * done?
    
    * upgrade packages

---
+++ util/pypi.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+home = os.path.expanduser("~")
+path = os.path.join(home, '.pypirc')
+token = os.environ.get('PYPI')
+test_token = os.environ.get('PYPI_TEST')
+
+with open(path, 'w') as file:
+    if token:
+        file.write('[pypi]\n')
+        file.write('  username = __token__\n')
+        file.write(f'  password = {token}\n')
+    if test_token:
+        file.write('[testpypi]\n')
+        file.write('  username = __token__\n')
+        file.write(f'  password = {test_token}\n')
