3463c23bd7cb855f289dae988da6aefa3cc5ee6d
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2021-01-12 03:57:08 -0500

    💱 fix(ohlc + intra): crypto datetime 💱 (#76)

--- scripts/update_intraday.py
+++ scripts/update_intraday.py
@@ -60,7 +60,7 @@ def update_poly_crypto_intraday():
     for idx, symbol in enumerate(crypto_symbols):
         try:
             poly_crypto.save_intraday(
-                symbol=symbol, timeframe='1d',
+                symbol=symbol, timeframe='3d',
                 retries=1 if C.TEST else C.DEFAULT_RETRIES)
         except Exception as e:
             print(f'Polygon.io intraday update failed for {symbol}.')
