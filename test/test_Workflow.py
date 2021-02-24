6ac1fdb1313c4642b787dfe4ea3798431c17a43c
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2021-02-23 22:50:52 -0500

    🧟 fix: zombie processes 🧟 (#94)
    * changes
    
    * #patch
    
    * more test coverage
    
    * fix test

--- test/test_Workflow.py
+++ test/test_Workflow.py
@@ -1,4 +1,5 @@
 import sys
+import pytest
 from datetime import datetime
 sys.path.append('src')
 from Workflow import Flow  # noqa autopep8
@@ -11,9 +12,8 @@ class TestWorkFlow:
     def test_get_workflow_start_time(self):
         assert flow.get_workflow_start_time('dividends') == datetime(
             now.year, now.month, 1, 12)
+        with pytest.raises(AttributeError):
+            assert flow.get_workflow_start_time('build')
 
     def test_is_workflow_running(self):
-        pass
-
-    def test_is_any_workflow_running(self):
-        pass
+        assert not flow.is_workflow_running('sentiment1')
