ad51641183a0cb9699725320f133410a09f8b076
Krish Suchak <suchak.krish@gmail.com>
2021-07-21 04:59:01 -0400

    fix get-loc

--- util/get_loc.sh
+++ util/get_loc.sh
@@ -1,5 +1,5 @@
 # get total lines of code
 
-git ls-files | grep -v 'img/*' | xargs cat | wc -l
+git ls-files | grep -v 'img/*' | grep -v 'ta-lib*' | xargs cat | wc -l
 # git ls-files | xargs wc -l
 # git diff --stat `git hash-object -t tree /dev/null`
