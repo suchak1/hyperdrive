a8e60f865e5579988fc8310a264b5fbeb4b91fa7
Krish Suchak <suchak.krish@gmail.com>
2021-10-09 23:11:30 -0400

    working on undersample

--- src/History.py
+++ src/History.py
@@ -4,11 +4,12 @@ import vectorbt as vbt
 import Constants as C
 from scipy.signal import argrelextrema
 from DataSource import MarketData
+from sklearn.model_selection import train_test_split
 
 
 class Historian:
     def buy_and_hold(self, symbol, timeframe, provider='polygon'):
-        # returns a portfolio
+        # returns a portfolio based on buy and hold strategy
         md = MarketData()
         md.provider = provider
         ohlc = md.get_ohlc(symbol, timeframe)
@@ -17,7 +18,12 @@ class Historian:
             close, init_cash=1000, freq='D')
         return portfolio
 
-    # function to create portfolio from signals & closes
+    def create_portfolio(self, close, signals):
+        # returns a portfolio based on signals
+        portfolio = vbt.Portfolio.from_signals(
+            close, signals, ~signals, init_cash=1000, freq='D'
+        )
+        return portfolio
 
     def fill(self, arr, method='ffill', type='bool'):
         # forward fills or nearest fills an array
@@ -44,3 +50,66 @@ class Historian:
         signals[maxs] = False
 
         return self.fill(signals, method=method)
+
+    def generate_random(self, close, num=10**4):
+        # generate random strategies
+        good_signals = []
+        portfolios = []
+        sortinos = []
+        calmars = []
+        num_strats = num
+        top_n = 25
+        prob = self.get_optimal_signals(close).mean()
+
+        for _ in range(num_strats):
+            signals = pd.DataFrame.vbt.signals.generate_random(
+                (len(close), 1), prob=prob)[0]
+            portfolio = vbt.Portfolio.from_signals(
+                close, signals, ~signals, init_cash=1000, freq='D')
+            sortinos.append(portfolio.sortino_ratio())
+            calmars.append(portfolio.calmar_ratio())
+            good_signals.append(signals)
+            portfolios.append(portfolio)
+
+        top_s = set(np.argpartition(sortinos, -top_n)[-top_n:])
+        top_c = set(np.argpartition(calmars, -top_n)[-top_n:])
+        top_idxs = top_s.intersection(top_c)
+
+        portfolios = [portfolio for idx, portfolio in enumerate(
+            portfolios) if idx in top_idxs]
+        good_signals = [signal for idx, signal in enumerate(
+            good_signals) if idx in top_idxs]
+        return good_signals
+
+    def undersample(self, X, y, to_skip=13):
+        # undersample and split train / test data
+        df = pd.DataFrame(X)
+        df['y'] =
+        X = X.tail(len(X) - to_skip)
+        y = pd.DataFrame(y)
+        y = y.tail(len(y) - to_skip)
+        X_train, X_test, y_train, y_test = \
+            train_test_split(X, y, test_size=.2)
+        train_true = 0
+        train_false = 0
+        X_train_new = []
+        y_train_new = []
+        train_num = len(y_train) - sum(y_train)
+        for idx, y in enumerate(y_train):
+            if y and train_true < train_num:
+                X_train_new.append(X_train[idx])
+                y_train_new.append(y_train[idx])
+                train_true += 1
+            elif not y and train_false < train_num:
+                X_train_new.append(X_train[idx])
+                y_train_new.append(y_train[idx])
+                train_false += 1
+        X_train = X_train_new
+        y_train = y_train_new
+
+        return X_train, X_test, y_train, y_test
+    # def run classifiers
+
+    # def plot_2d combos:
+
+    # def plot 3d combos
--- test/test_History.py
+++ test/test_History.py
@@ -1,9 +1,19 @@
 import sys
+import numpy as np
 sys.path.append('src')
 from History import Historian  # noqa autopep8
 
 
 hist = Historian()
+ls = [np.nan, True, True, np.nan, False, np.nan, np.nan, np.nan, True]
+fs = [True,   True, True, True,   False, False,  False,  False,  True]
+ns = [True,   True, True, True,   False, False,  False,  True,   True]
+arr = np.array(ls)
+test_ffill = np.array(fs)
+test_nfill = np.array(ns)
+
+
+close = np.array([3, 2, 5, 1, 100, 75, 50, 25, 1])
 
 
 class TestHistorian:
@@ -11,8 +21,22 @@ class TestHistorian:
         stats = hist.buy_and_hold('X%3ABTCUSD', '1y').stats()
         assert 'Sortino Ratio' in stats
 
+    def test_create_portfolio(self):
+        stats = hist.create_portfolio(close, test_ffill).stats()
+        assert 'Sortino Ratio' in stats
+
     def test_fill(self):
-        pass
+        ffill = hist.fill(arr)
+        assert np.array_equal(ffill, test_ffill)
+        nfill = hist.fill(arr, 'nearest')
+        assert np.array_equal(nfill, test_nfill)
 
     def test_get_optimal_signals(self):
-        pass
+        f_signals = hist.get_optimal_signals(close, n=2, method='ffill')
+        assert np.array_equal(f_signals, test_ffill)
+        n_signals = hist.get_optimal_signals(close, n=2, method='nfill')
+        assert np.array_equal(n_signals, test_nfill)
+
+    def test_generate_random(self):
+        strats = hist.generate_random(close, num=10)
+        assert sum(strats) > 0 and len(strats)
