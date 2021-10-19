bd5488f61de234508c5d42761f0d7f015b47af53
Krish Suchak <suchak.krish@gmail.com>
2021-10-18 23:41:14 -0400

    clean up unnecessary lines

--- src/Exchange.py
+++ src/Exchange.py
@@ -1,5 +1,4 @@
 import os
-import math
 from pprint import pprint
 from binance import Client
 from binance.helpers import round_step_size
@@ -26,32 +25,36 @@ class Binance:
 
     def order(self, base, quote, side, spend_ratio=1, test=False):
         pair = f'{base}{quote}'
+        side = side.upper()
         order_type = self.client.ORDER_TYPE_MARKET
         params = {'symbol': pair, 'type': order_type}
         symbol_info = self.client.get_symbol_info(pair)
         pprint(symbol_info)
 
-        if side.lower() == 'sell':
+        if side == 'SELL':
             side = self.client.SIDE_SELL
             balance_label = base
-            # precision_label = 'baseAssetPrecision'
             quantity_label = 'quantity'
             filters = symbol_info['filters']
             for filter in filters:
                 if filter['filterType'] == 'LOT_SIZE':
-                    precision = float(filter['stepSize'])
-        else:
+                    step_size = float(filter['stepSize'])
+        elif side == 'BUY':
             side = self.client.SIDE_BUY
             balance_label = quote
-            # precision_label = 'quoteAssetPrecision'
             quantity_label = 'quoteOrderQty'
+            precision = int(symbol_info['quoteAssetPrecision'])
+        else:
+            raise Exception('Need to specify BUY or SELL side for order')
 
         balance = float(self.client.get_asset_balance(balance_label)['free'])
         amount = spend_ratio * balance
-        # precision = symbol_info[precision_label]
-        # quantity = "{:0.0{}f}".format(amount, precision)
-        # step_size = 0.0001
-        quantity = round_step_size(amount, step_size)
+
+        if side == 'BUY':
+            quantity = "{:0.0{}f}".format(amount, precision)
+        else:
+            quantity = round_step_size(amount, step_size)
+
         params[quantity_label] = quantity
         print(quantity_label, params[quantity_label])
 
@@ -63,15 +66,7 @@ class Binance:
         order = fx(**params)
         return order
 
-
-#     order = client.order_market_buy(
-#     symbol='BNBBTC',
-#     quantity=100)
-
-# order = client.order_market_sell(
-#     symbol='BNBBTC',
-#     quantity=100)
-# buy
-# sell
-# get price
+# write script that gets most recent data at 9pm est
+# predicts using model
+# writes that back to predict.csv
 # write successful orders to binance.csv
--- test/test_Exchange.py
+++ test/test_Exchange.py
@@ -2,7 +2,7 @@ from pprint import pprint
 import sys
 sys.path.append('src')
 from Exchange import Binance  # noqa autopep8
-bn = Binance(testnet=False)
+bn = Binance(testnet=True)
 
 
 class TestBinance:
@@ -13,35 +13,69 @@ class TestBinance:
         assert hasattr(bn, 'client')
 
     def test_order(self):
-        # _to = 'BTC'
         base = 'BTC'
-        quote = 'USD'
+        quote = 'USDT'
+        bn.order(base, quote, 'buy', 0.01, test=True)
 
-        print('\n')
+    # selling btc response
+    # {'symbol': 'BTCUSD', 'orderId': 664894061, 'orderListId': -1,
+    # 'clientOrderId': 'EACZgyd3kO4r28V2Che5x9', 'transactTime': 1634612257816,
+    # 'price': '0.0000', 'origQty': '0.00080000', 'executedQty': '0.00080000',
+    # 'cummulativeQuoteQty': '49.4641', 'status': 'FILLED', 'timeInForce':
+    # 'GTC', 'type': 'MARKET', 'side': 'SELL', 'fills': [{'price':
+    # '61830.1400', 'qty': '0.00080000', 'commission': '0.0500',
+    # 'commissionAsset': 'USD', 'tradeId': 24328534}]}
 
-        # order = bn.order(base, quote, 'sell', 1, test=True)
-        order = bn.order(base, quote, 'buy', 1)
+    # buying btc response
+    # {'symbol': 'BTCUSD', 'orderId': 664895034, 'orderListId': -1,
+    # 'clientOrderId': 'S3ALGacJPWtYZ8IKLnTAXE', 'transactTime': 1634612303994,
+    # 'price': '0.0000', 'origQty': '0.00079900', 'executedQty': '0.00079900',
+    # 'cummulativeQuoteQty': '49.3823', 'status': 'FILLED', 'timeInForce':
+    # 'GTC', 'type': 'MARKET', 'side': 'BUY', 'fills': [{'price':
+    # '61805.2400', 'qty': '0.00079900', 'commission': '0.00000080',
+    # 'commissionAsset': 'BTC', 'tradeId': 24328549}]}
 
-        # order = bn.client.order_market_buy(symbol='BNBBTC', quantity=1)
-        print(order)
-        # orders = bn.client.get_open_orders(symbol='BNBBTC')
-        # print(orders)
-        # orders = bn.client.get_open_orders(symbol='BNBUSDT')
-        # print(orders)
-
-        # prices = bn.client.get_all_tickers()
-        # print(prices)
-
-    def test_get_asset_balance(self):
-        balances = {}
-        for symbol in ['USDT', 'BNB', 'BTC', 'USD', 'BUSD', 'USDC']:
-            balances[symbol] = bn.client.get_asset_balance(symbol)
-        print('\n')
-        pprint(balances)
-
-    # def test_get_asset_balance(self):
-    #     balances = {}
-    #     for symbol in ['USDT', 'BNB', 'BTC', 'USD', 'BUSD', 'USDC']:
-    #         balances[symbol] = bn.get_asset_balance(symbol)
-    #     print('\n')
-    #     pprint(balances)
+    # symbol_info response
+    # {'baseAsset': 'BTC',
+    #  'baseAssetPrecision': 8,
+    #  'baseCommissionPrecision': 8,
+    #  'filters': [{'filterType': 'PRICE_FILTER',
+    #               'maxPrice': '100000.0000',
+    #               'minPrice': '0.0100',
+    #               'tickSize': '0.0100'},
+    #              {'avgPriceMins': 5,
+    #               'filterType': 'PERCENT_PRICE',
+    #               'multiplierDown': '0.2',
+    #               'multiplierUp': '5'},
+    #              {'filterType': 'LOT_SIZE',
+    #               'maxQty': '9000.00000000',
+    #               'minQty': '0.00000100',
+    #               'stepSize': '0.00000100'},
+    #              {'applyToMarket': True,
+    #               'avgPriceMins': 5,
+    #               'filterType': 'MIN_NOTIONAL',
+    #               'minNotional': '10.0000'},
+    #              {'filterType': 'ICEBERG_PARTS', 'limit': 10},
+    #              {'filterType': 'MARKET_LOT_SIZE',
+    #               'maxQty': '3200.00000000',
+    #               'minQty': '0.00000000',
+    #               'stepSize': '0.00000000'},
+    #              {'filterType': 'MAX_NUM_ORDERS', 'maxNumOrders': 200},
+    #              {'filterType': 'MAX_NUM_ALGO_ORDERS', 'maxNumAlgoOrders': 5}],
+    #  'icebergAllowed': True,
+    #  'isMarginTradingAllowed': False,
+    #  'isSpotTradingAllowed': True,
+    #  'ocoAllowed': True,
+    #  'orderTypes': ['LIMIT',
+    #                 'LIMIT_MAKER',
+    #                 'MARKET',
+    #                 'STOP_LOSS_LIMIT',
+    #                 'TAKE_PROFIT_LIMIT'],
+    #  'permissions': ['SPOT'],
+    #  'quoteAsset': 'USD',
+    #  'quoteAssetPrecision': 4,
+    #  'quoteCommissionPrecision': 2,
+    #  'quoteOrderQtyMarketAllowed': True,
+    #  'quotePrecision': 4,
+    #  'status': 'TRADING',
+    #  'symbol': 'BTCUSD'}
