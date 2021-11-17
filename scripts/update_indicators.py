83db2f8e4c7436f5368eab01c1355c6456ba9fe1
Krish Suchak <suchak.krish@gmail.com>
2021-11-17 05:18:21 -0500

    indicators

--- scripts/update_indicators.py
+++ scripts/update_indicators.py
@@ -5,11 +5,12 @@ from DataSource import Glassnode  # noqa autopep8
 from Constants import PathFinder  # noqa autopep8
 import Constants as C  # noqa autopep8
 
-
+counter = 0
 glass = Glassnode()
 
 try:
     glass.save_s2f_ratio(timeframe='max', retries=1 if C.TEST else 2)
+    counter += 1
 except Exception as e:
     print('Glassnode S2F update failed.')
     print(e)
@@ -20,6 +21,7 @@ finally:
 
 try:
     glass.save_diff_ribbon(timeframe='max', retries=1 if C.TEST else 2)
+    counter += 1
 except Exception as e:
     print('Glassnode Diff Ribbon update failed.')
     print(e)
@@ -30,6 +32,7 @@ finally:
 
 try:
     glass.save_sopr(timeframe='max', retries=1 if C.TEST else 2)
+    counter += 1
 except Exception as e:
     print('Glassnode SOPR update failed.')
     print(e)
@@ -37,3 +40,6 @@ finally:
     filename = PathFinder().get_sopr_path()
     if C.CI and os.path.exists(filename):
         os.remove(filename)
+
+if counter < 3:
+    exit(1)
