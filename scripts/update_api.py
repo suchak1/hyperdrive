0743d39546b6db5706154ae047f22f1026fb1eb6
Krish Suchak <suchak.krish@gmail.com>
2021-11-26 22:34:13 -0500

    update api

---
+++ scripts/update_api.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import pandas as pd
+sys.path.append('hyperdrive')
+from DataSource import MarketData  # noqa autopep8
+from History import Historian  # noqa autopep8
+import Constants as C  # noqa autopep8
+
+# run this script after ohlc (maybe during intra update)
+
+md = MarketData()
+md.provider = 'polygon'
+hist = Historian()
+
+symbol = os.environ['SYMBOL']
+signals_path = md.finder.get_signals_path()
+signals = md.reader.load_csv(signals_path)
+signals[C.TIME] = pd.to_datetime(signals[C.TIME])
+df = md.get_ohlc(symbol).merge(signals, on=C.TIME)
+# close = df[C.CLOSE].to_numpy()
+# signals = df[C.SIG].to_numpy()
+# pf = vbt.Portfolio.from_signals(
+#     close, signals, ~signals, init_cash=1000, freq='D', fees=0.001)
+holding_pf = hist.buy_and_hold(df[C.CLOSE])
+hyper_pf = hist.create_portfolio(df[C.CLOSE], df[C.SIG], 0.001)
+metrics = [
+    'Total Return [%]',
+    'Max Drawdown [%]',
+    'Win Rate [%]',
+    'Profit Factor',
+    'Sharpe Ratio',
+    'Sortino Ratio'
+]
+holding = {
+    'balances': list(holding_pf.value()),
+    'stats': dict(holding_pf.stats())
+}
+hyper = {
+    'balances': list(hyper_pf.value()),
+    'stats': dict(hyper_pf.stats())
+}
+
+holding_path = md.finder.get_api_path('holding')
+hyper_path = md.finder.get_api_path('hyper')
+md.writer.save_json(holding_path, holding)
+md.writer.save_json(hyper_path, hyper)
