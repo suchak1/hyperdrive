2755eec28d6ede3eb1420c5ab39d905abdd97932
Krish Suchak <suchak.krish@gmail.com>
2022-09-16 07:11:24 -0400

    make 2D folder

--- hyperdrive/Precognition.py
+++ hyperdrive/Precognition.py
@@ -1,23 +1,31 @@
 import numpy as np
 from sklearn.decomposition import PCA
-from FileOps import FileReader
+from FileOps import FileReader, FileWriter
 from Calculus import Calculator
 
 
 class Oracle:
     def __init__(self):
         self.reader = FileReader()
+        self.writer = FileWriter()
         self.calc = Calculator()
 
+    def get_filename(self, name):
+        return f'models/latest/{name}.pkl'
+
     def load_model_pickle(self, name):
-        filename = f'models/latest/{name}.pkl'
+        filename = self.get_filename(name)
         return self.reader.load_pickle(filename)
+
+    def save_model_pickle(self, name, data):
+        filename = self.get_filename(name)
+        return self.writer.save_pickle(filename, data)
 
     def predict(self, data):
         model = self.load_model_pickle('model')
         return model.predict(data)
 
-    def visualize(self, refinement, dimensions, X, increase_percent=0):
+    def visualize(self, X, dimensions, refinement, increase_percent=0):
         # recommended in the range [4, 100]
         num_points = refinement
         reducer = PCA(n_components=dimensions)
--- scripts/visualize.py
+++ scripts/visualize.py
@@ -9,10 +9,29 @@ oracle = Oracle()
 
 
 X = oracle.load_model_pickle('X')
-y = oracle.load_model_pickle('y')
+# y = oracle.load_model_pickle('y')
 
-# on line 43 of preds_v2.py
-# finish translating
+# 2D
+(
+    actual_2D,
+    centroid_2D,
+    radius_2D,
+    grid_2D,
+    preds_2D
+) = oracle.visualize(X=X, dimensions=2, refinement=4)
+
+
+oracle.save_model_pickle('2D/actual', actual_2D)
+oracle.save_model_pickle('2D/centroid', centroid_2D)
+oracle.save_model_pickle('2D/radius', radius_2D)
+oracle.save_model_pickle('2D/grid', grid_2D)
+oracle.save_model_pickle('2D/preds', preds_2D)
+
+
+# Don't actually need to save the radius => just need to use it to modify points that plot will use ultimately
+# Need to save final data plot will use after deciding shapes and processing
+
+
 # and then get 2d and 3d correct from visualize_plotly.ipynb
 # make 2d square?
 # use contour for 2d?
