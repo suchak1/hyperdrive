07e5bfe67f138d0cce6c86b092ce6a3d5a0eb5da
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2023-04-13 22:01:19 -0400

    🔕 feat(alerts): mute 🔕 (#232)
    * start notify
    
    * add notifications
    
    * implement pref exchange
    
    * #patch
    
    * handle low kraken bal
    
    * rename crypto file

--- util/decrypt.py
+++ util/decrypt.py
@@ -1,9 +1,8 @@
 import os
 import sys
-import base64
 from dotenv import load_dotenv, find_dotenv
-from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+sys.path.append('hyperdrive')
+from Crypt import Cryptographer  # noqa autopep8
 
 
 load_dotenv(find_dotenv('config.env'))
@@ -12,18 +11,9 @@ password = os.environ['RH_PASSWORD'].encode('UTF-8')
 salt = os.environ['SALT'].encode('UTF-8')
 filename = os.environ.get('FILE') or sys.argv[1]
 
-kdf = Scrypt(
-    salt=salt,
-    length=32,
-    n=2**14,
-    r=8,
-    p=1,
-)
-
-key = base64.urlsafe_b64encode(kdf.derive(password))
-f = Fernet(key)
+cryptographer = Cryptographer(password, salt)
 with open(f'{filename}.encrypted', 'rb') as file:
-    plaintext = f.decrypt(file.read()).decode('UTF-8')
+    plaintext = cryptographer.decrypt(file.read()).decode('UTF-8')
 
 with open(filename, 'w') as file:
     file.write(plaintext)
--- util/encrypt.py
+++ util/encrypt.py
@@ -1,9 +1,8 @@
 import os
 import sys
-import base64
 from dotenv import load_dotenv, find_dotenv
-from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+sys.path.append('hyperdrive')
+from Crypt import Cryptographer  # noqa autopep8
 
 
 load_dotenv(find_dotenv('config.env'))
@@ -12,18 +11,9 @@ password = os.environ['RH_PASSWORD'].encode('UTF-8')
 salt = os.environ['SALT'].encode('UTF-8')
 filename = os.environ.get('FILE') or sys.argv[1]
 
-kdf = Scrypt(
-    salt=salt,
-    length=32,
-    n=2**14,
-    r=8,
-    p=1,
-)
-
-key = base64.urlsafe_b64encode(kdf.derive(password))
-f = Fernet(key)
+cryptographer = Cryptographer(password, salt)
 with open(filename, 'r') as file:
-    ciphertext = f.encrypt(file.read().encode('UTF-8'))
+    ciphertext = cryptographer.encrypt(file.read().encode('UTF-8'))
 
 with open(f'{filename}.encrypted', 'wb') as file:
     file.write(ciphertext)
