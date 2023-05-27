a5332ec199e11faedfbae7c90b6c7a6c529055ef
Krish Suchak <suchak.krish@gmail.com>
2023-05-27 16:27:46 -0400

    write more tests

--- test/test_Transformer.py
+++ test/test_Transformer.py
@@ -11,7 +11,41 @@ encoder = NumpyEncoder()
 
 class TestNumpyEncoder:
     def test_default(self):
+        # list
         arr = np.array([True, False])
         with pytest.raises(TypeError):
             json.dumps(arr)
         assert json.dumps(NumpyEncoder().default(arr)) == '[true, false]'
+
+        # bool
+        val = np.True_
+        with pytest.raises(TypeError):
+            json.dumps(val)
+        assert json.dumps(NumpyEncoder().default(val)) == 'true'
+
+        # int
+        val = np.int64(1)
+        with pytest.raises(TypeError):
+            json.dumps(val)
+        assert json.dumps(NumpyEncoder().default(val)) == '1'
+
+        # # float
+        # val = np.float64(0.5)
+        # with pytest.raises(TypeError):
+        #     json.dumps(val)
+        # assert json.dumps(NumpyEncoder().default(val)) == '0.5'
+
+        # # complex
+        # val = np.complex64(1 + 2j)
+        # with pytest.raises(TypeError):
+        #     json.dumps(arr)
+        # assert json.dumps(NumpyEncoder().default(val)
+        #                   ) == '{"real": 1, "imag": 2}'
+
+        # none
+
+        # other
+        arr = []
+        with pytest.raises(TypeError):
+            json.dumps(NumpyEncoder().default(arr))
+        assert json.dumps(arr) == '[]'
