0422d37cc097506798ca0ff15d3629369cee2d5f
Krish Suchak <suchak.krish@gmail.com>
2023-07-03 00:40:17 -0400

    update dep script

--- util/update.py
+++ util/update.py
@@ -24,29 +24,40 @@ packages_to_skip = [
 ]
 
 with open(filename, 'r') as file:
-    pattern = r'(\S*)\s?==\s?(\S*)'
+    for line in file:
+        
+    pattern = r'^(\S*)(\s?==\s?)?(\S*)$'
     packages = re.findall(pattern, file.read())
-    for package, version in packages:
-        response = requests.get(f'https://pypi.org/pypi/{package}/json')
-        keys = response.json()['releases'].keys()
-        releases = [key for key in keys if key.replace('.', '').isdigit()]
-        latest = sorted(
-            releases,
-            key=lambda release: [
-                int(number) for number in release.split('.')
-            ]).pop()
-        if latest != version and package not in packages_to_skip:
-            print(f'Upgrading {package} ({version} => {latest})')
-            CI = os.environ.get('CI')
-            python = 'python' if CI else 'python3'
-            cmd = f'{python} -m pip install {package}=={latest}'
-            code = subprocess.run(cmd, shell=True).returncode
-            if code:
-                exit(code)
-            version = latest
+    for package in packages:
+        if len(package) == 1:
+            new_packages.append((package))
+        else:
+            package, _, version = package
+            response = requests.get(f'https://pypi.org/pypi/{package}/json')
+            keys = response.json()['releases'].keys()
+            releases = [key for key in keys if key.replace('.', '').isdigit()]
+            latest = sorted(
+                releases,
+                key=lambda release: [
+                    int(number) for number in release.split('.')
+                ]).pop()
+            if latest != version and package not in packages_to_skip:
+                print(f'Upgrading {package} ({version} => {latest})')
+                CI = os.environ.get('CI')
+                python = 'python' if CI else 'python3'
+                cmd = f'{python} -m pip install {package}=={latest}'
+                # code = subprocess.run(cmd, shell=True).returncode
+                # if code:
+                #     exit(code)
+                version = latest
 
-        new_packages.append((package, version))
-
+            new_packages.append((package, version))
+print(new_packages)
 with open(filename, 'w') as file:
-    for package, version in new_packages:
-        file.write(f'{package} == {version}\n')
+    for package in new_packages:
+        print(package)
+        if len(package) == 1:
+            file.write(f'{package}\n')
+        else:
+            package, _, version = package
+            file.write(f'{package} == {version}\n')
