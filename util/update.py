567e9dae8e4791bc4ae70f85e34e1d6bb6fca459
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2023-07-03 04:17:10 -0400

    🔮 fix(data): glassnode 🔮 (#236)
    * update dep script
    
    * update script complete
    
    * revert requirements.txt
    
    * change datasource
    
    * update instances
    
    * #patch use chrome browser
    
    * add options
    
    * add env var
    
    * use default asset param
    
    * fix
    
    * fix predict script and update deps

--- util/update.py
+++ util/update.py
@@ -22,31 +22,38 @@ packages_to_skip = [
     # 'DataFrame' object has no attribute 'iteritems' => with pandas >= 2
     'pandas'
 ]
+pattern = r'(\S*)\s?==\s?(\S*)'
 
 with open(filename, 'r') as file:
-    pattern = r'(\S*)\s?==\s?(\S*)'
-    packages = re.findall(pattern, file.read())
-    for package, version in packages:
-        response = requests.get(f'https://pypi.org/pypi/{package}/json')
-        keys = response.json()['releases'].keys()
-        releases = [key for key in keys if key.replace('.', '').isdigit()]
-        latest = sorted(
-            releases,
-            key=lambda release: [
-                int(number) for number in release.split('.')
-            ]).pop()
-        if latest != version and package not in packages_to_skip:
-            print(f'Upgrading {package} ({version} => {latest})')
-            CI = os.environ.get('CI')
-            python = 'python' if CI else 'python3'
-            cmd = f'{python} -m pip install {package}=={latest}'
-            code = subprocess.run(cmd, shell=True).returncode
-            if code:
-                exit(code)
-            version = latest
+    for line in file:
+        match = re.match(pattern, line)
+        line = line.strip()
+        if match:
+            package, version = match.groups()
+            response = requests.get(f'https://pypi.org/pypi/{package}/json')
+            keys = response.json()['releases'].keys()
+            releases = [key for key in keys if key.replace('.', '').isdigit()]
+            latest = sorted(
+                releases,
+                key=lambda release: [
+                    int(number) for number in release.split('.')
+                ]).pop()
+            if latest != version and package not in packages_to_skip:
+                print(f'Upgrading {package} ({version} => {latest})')
+                CI = os.environ.get('CI')
+                python = 'python' if CI else 'python3'
+                cmd = f'{python} -m pip install {package}=={latest}'
+                code = subprocess.run(cmd, shell=True).returncode
+                if code:
+                    exit(code)
+                version = latest
 
-        new_packages.append((package, version))
+            new_packages.append({'package': package, 'version': version})
+        elif line:
+            new_packages.append({'package': line})
 
 with open(filename, 'w') as file:
-    for package, version in new_packages:
-        file.write(f'{package} == {version}\n')
+    for package in new_packages:
+        prefix = package['package']
+        suffix = f"{' == ' + package['version'] if 'version' in package else ''}\n"
+        file.write(f"{prefix}{suffix}")
